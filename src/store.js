73d474ff8530c411767c34a20485875db60d460a
visor517 <visor517@yandex.ru>
2023-05-11 01:49:27 +0300

    task1 select logic

--- src/store.js
+++ src/store.js
@@ -70,6 +70,9 @@ class Store {
         if (item.code === code) {
           item.selected = !item.selected;
         }
+        else {
+          item.selected = false;
+        }
         return item;
       })
     })
