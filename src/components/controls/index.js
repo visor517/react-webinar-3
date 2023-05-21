506d8bcdcf6e553ab5b7738c430ce8120d37be36
visor517 <visor517@yandex.ru>
2023-05-21 23:53:44 +0300

    added pluaral to stat

--- src/components/controls/index.js
+++ src/components/controls/index.js
@@ -8,7 +8,9 @@ function Controls({onTaggle, basket}){
 
   return (
     <div className='Controls'>
-      <div className='ControlsStat'>В корзине: <span>{basket.length ? `${basket.length} товаров / ${amount} ₽` : 'пусто'}</span></div>
+      <div className='Controls-stat'>
+        В корзине: <span className="Controls-products">{basket.length ? `${basket.length} ${plural(basket.length, {one: 'товар', few: 'товара', many: 'товаров'})} / ${amount} ₽` : 'пусто'}</span>
+      </div>
       <button onClick={() => onTaggle()}>Перейти</button>
     </div>
   )
